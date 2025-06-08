b6838185e5cf0bddfbee9f5c5cb15a3c43791c0a
Kaly DIALLO <kalydiallo@MacBook-Pro-de-Kaly.local>
2025-06-08 22:47:10 +0000

    fix cors url

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://enens-api.onrender.com",
-    credentials: true
+    origin: "http://localhost:5173",
+    credentials: true,
   })
   await app.listen(process.env.PORT ?? 3000);
 }
