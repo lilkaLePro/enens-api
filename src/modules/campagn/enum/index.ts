f0539c58c1e52ff2cfc9471b9f9e1698abcf4d7a
kaly05 <kaly.diallo@paps-app.com>
2026-07-11 20:56:45 +0000

    firt commit

--- src/modules/campagn/enum/index.ts
+++
@@ -1,8 +0,0 @@
-import { registerEnumType } from "@nestjs/graphql";
-
-export enum CAMPAGN_TYPE {
-  PROMO_CAMPAGNE = "PRELUNCH",
-  FUND_RAISING_CAMPAGNE = "FUND_RAISING_CAMPAGNE"
-}
-
-registerEnumType(CAMPAGN_TYPE, {name: "CAMPAGN_TYPE"});
